be4dcf7119f210ae60c2d0f057ce6750d6744d6c
ireshjayawardana <ireshjayawardana@gmail.com>
2021-03-22 18:15:20 +0530

    height adjest
    Signed-off-by: ireshjayawardana <ireshjayawardana@gmail.com>

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -705,7 +705,7 @@
 #define DELTA_DIAGONAL_ROD 280.0        // (mm)
 
 // Distance between bed and nozzle Z home position
-#define DELTA_HEIGHT 880.00             // (mm) Get this value from G33 auto calibrate
+#define DELTA_HEIGHT 650.00             // (mm) Get this value from G33 auto calibrate
 
 #define DELTA_ENDSTOP_ADJ { 0.0, 0.0, 0.0 } // Get these values from G33 auto calibrate
 
